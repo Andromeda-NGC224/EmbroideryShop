fe2eaf368fd6a32dea93fbc32b8e114d77fb8a63
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2023-11-25 00:00:31 +0200

    added background image for mobile menu and script for scroll

--- src/main.js
+++ src/main.js
@@ -15,6 +15,44 @@ function closeMobileMenu(event) {
 const mobileMenuClose = document.querySelector('.mobile-menu-btn');
 mobileMenuClose.addEventListener('click', closeMobileMenu);
 
+// HEADER SCROLL
+
+const scrollHeader = document.querySelector('.header');
+
+const addHeaderScroll = () => {
+    scrollHeader.classList.add('scrolled');
+}
+
+const removeHeaderScroll = () => {
+    scrollHeader.classList.remove('scrolled');
+}
+
+window.addEventListener('scroll', function () {
+    let getScrollPosition = window.scrollY;
+
+    if (getScrollPosition > 50) {
+        console.log(addHeaderScroll);
+        addHeaderScroll();
+    } else {
+        removeHeaderScroll();
+    }
+})
+
+// SMOOTH SCROLLING WHEN CLICKING AN ANCHOR LINK
+
+const anchorlinks = document.querySelectorAll('a[href^="#"]');
+for (let anchor of anchorlinks) {
+    anchor.addEventListener('click', function (event) {
+        const value = anchor.getAttribute('href');
+        const target = document.querySelector(value);
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, value)
+        event.preventDefault()
+    })
+}
 
 
 
